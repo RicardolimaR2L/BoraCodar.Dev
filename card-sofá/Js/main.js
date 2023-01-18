ffd1ae17084cf8a9b2763a8e64a89bcc33b0e6d2
ricardo <ricardolimadelemos.trab@gmail.com>
2023-01-18 12:38:10 -0300

    Innitial commit

---
+++ card-sofá/Js/main.js
@@ -0,0 +1,9 @@
+var sofa1 = "imagem/sofá.svg";
+var SofaGif = "imagem/sofá.gif";
+
+function trocar(){    
+  document.getElementById("img-sofa").src = sofa1;
+  let aux = sofa1;
+  sofa1 = SofaGif;
+  SofaGif = aux;
+}
